28d4a4e98c4a3c8f7dd36859b900637ecc179033
RodroVMS <democlesvanshepars@gmail.com>
2022-01-11 15:36:31 -0500

    Refactoring and bug fix

--- src/World/Board.hs
+++ src/World/Board.hs
@@ -1,35 +1,34 @@
 module World.Board where
 
-import Data.Maybe (fromJust, isJust, isNothing)
+import Data.Maybe (catMaybes, fromJust, isJust, isNothing)
 import World.Objects
 
-moves :: Object -> Board -> [Maybe (Action Position)]
-moves obj board
-  | carries = map (move obj board) directions2
-  | isKid || isRobot = map (move obj board) directions
-  | otherwise = error "Movement is not defined for this object"
-  where
-    objType = typex obj
-    carries = objType == Robot (Just Kid)
-    isRobot = objType == Robot Nothing
-    isKid = objType == Kid
+moves :: Object -> Board -> [Action Position]
+moves obj board =
+  let allMoves = case typex obj of
+        Robot (Just Kid) -> map (move obj board) directions2
+        Robot Nothing -> map (move obj board) directions
+        Kid -> map (move obj board) directions
+        _ -> error "Movement is not defined for this object"
+   in catMaybes allMoves
 
 move :: Object -> Board -> Position -> Maybe (Action Position)
 move (Object Kid pos) board dir
   | isNothing cell || cannotMove = Nothing
   | null objTypes = Just (Move goto)
   | objTypes == [Obstacle] =
-    case dir of
-      Position i 0 ->
-        let (case1, len1) = canPush (getRow board (row pos)) (col pos) i
-         in if case1
-              then Just (Push goto (Position i len1))
-              else Nothing
-      Position 0 j ->
-        let (case2, len2) = canPush (getCol board (col pos)) (row pos) j
-         in if case2
-              then Just (Push goto (Position len2 j))
-              else Nothing
+    let (rowPos, colPos) = tupleFromPosition pos
+     in case dir of
+          Position i 0 ->
+            let (case1, len1) = canPush (getCol board colPos) rowPos i
+             in if case1
+                  then Just (Push goto (Position (rowPos + i * len1) colPos))
+                  else Nothing
+          Position 0 j ->
+            let (case2, len2) = canPush (getRow board rowPos) colPos j
+             in if case2
+                  then Just (Push goto (Position rowPos (colPos + j * len2)))
+                  else Nothing
   | otherwise = Nothing
   where
     goto = pos + dir
@@ -52,7 +51,7 @@ move (Object (Robot carries) pos) board dir
         && (fromJust (board ! pos) == [Dirt, Robot Nothing])
     canDrop =
       (dir == Position 0 0)
-        && (fromJust (board ! pos) == [Robot (Just Kid)])
+        && (fromJust (board ! pos) == [Crib, Robot (Just Kid)])
 
 applyMove :: Object -> Action Position -> Board -> Board
 applyMove obj (Clean pos) board = board *- Object Dirt pos
@@ -71,6 +70,6 @@ canPush xs posi diri
 movableObstacle :: [[ObjectType]] -> Int -> (Bool, Int)
 movableObstacle [] acc = (False, acc)
 movableObstacle (x : xs) acc
-  | null x = (True, acc)
+  | null x = (True, acc + 1)
   | x == [Obstacle] = movableObstacle xs (acc + 1)
   | otherwise = (False, acc)

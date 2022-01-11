8e2dda329833d6e5f496449e58dffebc090846a7
RodroVMS <democlesvanshepars@gmail.com>
2022-01-11 16:05:59 -0500

    Add more kid movement tests

--- test/WorldSpec.hs
+++ test/WorldSpec.hs
@@ -8,8 +8,9 @@ import World.Objects
 
 boardPrimitiveTest :: SpecWith ()
 boardPrimitiveTest = describe "Basic board function test" $ do
-  it "Should return an empty board row" $
-    getRow board 1 `shouldBe` [[] | _ <- [1 .. rows]]
+  it "Should return a column" $
+    getCol (board *++ makeMany Obstacle [(i, 1) | i <- [0 .. rows - 1]]) 1
+      `shouldBe` [[Obstacle] | _ <- [1 .. rows]]
   it "Obstacle should be movable" $
     movableObstacle pushable 0 `shouldBe` (True, 4)
   it "Obstacle should not be movable" $
@@ -31,12 +32,38 @@ kidTest = describe "Kid movement" $ do
   it "Should move or push obstacles" $
     let kid = make Kid (1, 1)
         obstacles = makeMany Obstacle [(1, 2), (1, 3), (1, 4)]
-        board = newBoard 5 6 *++ (kid : obstacles)
-     in moves kid board
+     in moves kid (board *++ (kid : obstacles))
           `shouldBe` [ Move (pos (2, 1)),
                        Move (pos (0, 1)),
                        Push (pos (1, 2)) (pos (1, 5)),
                        Move (pos (1, 0))
                      ]
+  it "Should push in every direction" $
+    let kid = make Kid (2, 2)
+        obstacles = makeMany Obstacle [(2, 1), (1, 2), (3, 2), (2, 3), (2, 4)]
+     in moves kid (board *++ (kid : obstacles))
+          `shouldBe` [ Push (pos (3, 2)) (pos (4, 2)),
+                       Push (pos (1, 2)) (pos (0, 2)),
+                       Push (pos (2, 3)) (pos (2, 5)),
+                       Push (pos (2, 1)) (pos (2, 0))
+                     ]
+  it "Should not be able to move" $
+    let kid = make Kid (2, 2)
+        obstacles =
+          makeMany
+            Obstacle
+            [ (2, 0),
+              (2, 1),
+              (0, 2),
+              (1, 2),
+              (3, 2),
+              (4, 2),
+              (2, 3),
+              (2, 4),
+              (2, 5)
+            ]
+     in moves kid (board *++ (kid : obstacles)) `shouldBe` []
+  where
+    board = newBoard 5 6
 
 pos = positionFromTuple

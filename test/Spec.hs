cec3bd319c33b38fb8c5a9b6ac881ea9730bf6af
rodrigo-pino <rodrodpino@gmail.com>
2022-01-16 15:40:45 -0500

    Minor update

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,8 +4,11 @@ import WorldSpec
 
 main :: IO ()
 main = do
+  -- Board Rules Tests
   hspec boardPrimitiveTest
   hspec kidTest
   hspec robotWithoutKidTest
   hspec robotWithKidTest
+  hspec actionApplicationTest
+  -- Visual Tests
   hspec visualTest

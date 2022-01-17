367d2eefff8c8c87e763ac7a6efd9fa5b94bee86
rodrigo-pino <rodrodpino@gmail.com>
2022-01-17 16:31:14 -0500

    Update tests to work with updated code

--- test/TaskHandlingSpec.hs
+++ test/TaskHandlingSpec.hs
@@ -11,12 +11,13 @@ import World.Objects
 findTaskTest = describe "Detect all tasks on board" $ do
   it "Should detect a cleaning task" $
     let dirt = make Dirt (5, 5)
-     in getTasks (board *+ dirt) `shouldBe` [Task {target = dirt, solvers = []}]
-  it "Should detect a moving kid to crib task" $
+     in getTasks (board *+ dirt) [] `shouldBe` [Task {target = dirt, solvers = []}]
+  it "Should detect a crib task" $
     let crib = make Crib (5, 5)
-     in getTasks (board *+ crib) `shouldBe` [Task {target = crib, solvers = []}]
+     in getTasks (board *+ crib) [] `shouldBe` [Task {target = crib, solvers = []}]
   where
     board = newBoard 10 10
 
--- optimizeTest = describe "Optimal distribution of tasks" $ do
--- it ""
+optimizeTest = describe "Optimal distribution of tasks" $ do
+  it "Should assing the agent the fastest to complete task" $
+    3 `shouldBe` 3

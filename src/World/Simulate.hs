f624d3eb0a9917e617593c5dd83cb20215096daa
rodrigo-pino <rodrodpino@gmail.com>
2022-01-21 19:13:47 -0500

    Add simulation loop for world

--- src/World/Simulate.hs
+++ src/World/Simulate.hs
@@ -1,45 +1,75 @@
 module World.Simulate where
 
+import Data.Map (Map (..))
+import qualified Data.Map as Map
+import Data.Maybe (fromJust)
 import System.Random
-import World.Objects
+import World.Board
+import World.Objects hiding ((!))
+
+type KidCluster = Map Object Int
 
 worldInit :: Int -> Int -> [Object] -> Board
 worldInit rows cols objects = Board objects rows cols
 
-worldSim :: Board -> Board
-worldSim board = board
+worldSim :: Board -> StdGen -> Bool -> (Board, StdGen)
+worldSim board g True = shuffle board g
+worldSim board g False = moveKids board g
 
-moveKids :: Board -> Board
-moveKids board = board
+moveKids :: Board -> StdGen -> (Board, StdGen)
+moveKids board g =
+  let kidCluster = getKidCluster board
+   in Map.foldlWithKey toMoveOrNotMove (board, g) kidCluster
+  where
+    -- acc -> key -> val
+    toMoveOrNotMove :: (Board, StdGen) -> Object -> Int -> (Board, StdGen)
+    toMoveOrNotMove (board, g) kid kidsNear
+      | nextStep >= length posMoves = (board, newG)
+      | otherwise =
+        let updatedBoard = applyMove kid nextMove board
+            emptyNear = emptyAround updatedBoard (position kid)
+            dirtAmount = case kidsNear of
+              0 -> 1
+              1 -> 3
+              _ -> 6
+         in toDirt updatedBoard emptyNear dirtAmount newG
+      where
+        posMoves = moves kid board
+        nextMove = head posMoves
+        (nextStep, newG) = randomR (0, 4) g
 
-generateDirt :: Board -> Board
-generateDirt board = board
+    toDirt :: Board -> [Position] -> Int -> StdGen -> (Board, StdGen)
+    toDirt board positions amount g
+      | null positions || amount == 0 = (board, g)
+      | otherwise =
+        let (pos, remPositions, newG) = getRandom positions g
+         in toDirt (board *+ Object Dirt pos) remPositions (amount - 1) newG
 
 shuffle :: Board -> StdGen -> (Board, StdGen)
 shuffle board g = (Board newElems n m, newG)
   where
     (newElems, newG) = relocateObjects boardElems allPos g
-    allPos = [(i, j) | i <- [0 .. n - 1], j <- [0 .. m - 1]]
+    allPos = [Position i j | i <- [0 .. n - 1], j <- [0 .. m - 1]]
     boardElems = elems board
     n = maxRows board
     m = maxCols board
 
-relocateObjects :: [Object] -> [(Int, Int)] -> StdGen -> ([Object], StdGen)
+relocateObjects :: [Object] -> [Position] -> StdGen -> ([Object], StdGen)
 relocateObjects [] _ g = ([], g)
 relocateObjects (obj : objects) positions g = (newObj : newObjs, lastG)
   where
-    (newPos, remPositions, newG) = getPosition positions g
-    newObj = make (typex obj) newPos
+    (newPos, remPositions, newG) = getRandom positions g
+    newObj = Object (typex obj) newPos
     (newObjs, lastG) = relocateObjects objects remPositions newG
 
-getPosition :: (Num a, Num b) => [(a, b)] -> StdGen -> ((a, b), [(a, b)], StdGen)
-getPosition [] _ = error "Out of positions"
-getPosition positions g = (selectedPos, remPositions, newG)
+getRandom :: [a] -> StdGen -> (a, [a], StdGen)
+getRandom [] _ = error "Out of positions"
+getRandom positions g = (selectedPos, remPositions, newG)
   where
     nextP :: Int
     (nextP, newG) = randomR (0, length positions) g
 
-    (selectedPos, _, remPositions) = foldl extract ((-1, -1), 0, []) positions
+    (Just selectedPos, _, remPositions) = foldl extract (Nothing, 0, []) positions
     extract (ext, i, list) val
-      | i == nextP = (val, i + 1, list)
+      | i == nextP = (Just val, i + 1, list)
       | otherwise = (ext, i + 1, val : list)

403f63f5e39f87423963ad7119e1f99a0b129f6c
rodrigo-pino <rodrodpino@gmail.com>
2022-02-01 11:23:22 -0500

    Add new path calculation type

--- src/Agent/Logic/Pathfinding/PathCalculation.hs
+++ src/Agent/Logic/Pathfinding/PathCalculation.hs
@@ -1,6 +1,8 @@
 module Agent.Logic.Pathfinding.PathCalculation where
 
 import Agent.Objects
+import Data.Maybe (fromJust)
+import Debug.Trace (trace)
 import World.Objects
 
 data PathCalcType = ShortestPath | BestReward
@@ -9,4 +11,22 @@ actionCalc :: PathCalcType -> Board -> Natural -> Action Position -> Natural
 actionCalc typex board acc move =
   case typex of
     ShortestPath -> acc + 1
-    BestReward -> error "Not yet implemented"
+    BestReward -> bestRewardCalc board acc move
+
+bestRewardCalc :: Board -> Natural -> Action Position -> Natural
+bestRewardCalc board acc@(Natural val) move =
+  case move of
+    Drop pos
+      | fromJust (board ! pos) == [Crib, Robot (Just Kid)] ->
+        min
+          (Natural (val `div` 2)) -- to reward long paths
+          (acc - baseMoveCost * 5) -- to reward short paths
+      | otherwise -> acc + baseMoveCost * (1 + 2 * kidsAround pos)
+    Grab pos -> acc + baseMoveCost * (1 - 2 * kidsAround pos)
+    Clean pos -> acc - (2 * baseMoveCost)
+    Move pos -> acc + baseMoveCost
+    _ -> error ("Unexpected movement type: " ++ show move)
+  where
+    -- Helpers
+    baseMoveCost = Natural 100
+    kidsAround position = Natural (length (filter ([Kid] ==) (adyacentsTo board position)))

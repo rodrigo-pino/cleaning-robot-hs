08a162df6c4251303f1378343c01548c65d72a64
rodrigo-pino <rodrodpino@gmail.com>
2022-02-03 13:32:32 -0500

    Add parallel path calculation in Agent simulation

--- src/Agent/Simulate.hs
+++ src/Agent/Simulate.hs
@@ -1,10 +1,11 @@
 module Agent.Simulate where
 
 import Agent.Logic.Pathfinding.Algorithm (localApplyMove)
-import Agent.Logic.Pathfinding.Find (findTarget)
+import Agent.Logic.Pathfinding.Find (findTarget, findTargets)
 import Agent.Logic.Pathfinding.PathCalculation
 import Agent.Logic.TaskHandling (assignTasks)
 import Agent.Objects
+import Control.Parallel (par, pseq)
 import Data.Maybe (fromJust, isJust, isNothing)
 import Debug.Trace (trace)
 import GHC.List (foldl')
@@ -28,23 +29,54 @@ agentSim calcType board = loop board []
             updMovedAgents,
             updNotMovedAgents
             ) = moveAgents board movedAgents notMovedAssignedAgents calcType
-          [l1, l2] = map length [notMovedAgents, updNotMovedAgents]
-       in if l1 == l2
-            then (updBoard, movedAgents ++ updMovedAgents ++ updNotMovedAgents)
-            else loop updBoard (movedAgents ++ updMovedAgents) updNotMovedAgents
+       in if notMovedAgents %== updNotMovedAgents
+            then (updBoard, updMovedAgents ++ updNotMovedAgents)
+            else loop updBoard updMovedAgents updNotMovedAgents
 
 moveAgents :: Board -> [Agent] -> [Agent] -> PathCalcType -> (Board, [Agent], [Agent])
-moveAgents board _ [] _ = (board, [], [])
-moveAgents board movedAgents (ag : agents) calcType =
+moveAgents board movedAgents toMoveAgents calcType =
+  (newBoard, movedAgents ++ movedAg, notMovedAg)
+  where
+    -- Apply agents actions sequentially
+    (newBoard, movedAg, notMovedAg) = g board toMoveAgents paths
+    g board [] [] = (board, [], [])
+    g board (ag : ags) (path : paths)
+      | null path =
+        let (resBoard, resMovedAg, resNotMovedAg) = g board ags paths
+         in (resBoard, resMovedAg, unassingAgent ag : resNotMovedAg)
+      | otherwise =
+        let (resBoard, resMovedAg, resNotMovedAg) = g updBoard ags paths
+         in (resBoard, updAg : resMovedAg, resNotMovedAg)
+      where
+        action = head path
+        (updBoard, updAg) = agentApplyMove board ag action
+
+    -- Finding path for each agent and its objective using parallel execution
+    paths = f toMoveAgents -- findTargets board toMoveAgents calcType
+    f [] = []
+    f (ag : ags)
+      | (isNothing . task) ag = [] : ps
+      | otherwise = p `par` (ps `pseq` p : ps)
+      where
+        ps = f ags
+        agentlessBoard = removeActiveAgents board ag allAgents
+        p = findTarget agentlessBoard ag calcType
+
+    -- helpers
+    allAgents = movedAgents ++ toMoveAgents
+
+moveAgentsOld :: Board -> [Agent] -> [Agent] -> PathCalcType -> (Board, [Agent], [Agent])
+moveAgentsOld board _ [] _ = (board, [], [])
+moveAgentsOld board movedAgents (ag : agents) calcType =
   if isNothing (task ag) || null actions
     then
       let unAg = unassingAgent ag
           (rBoard, rmovedAgents, rnotMovedAgents) =
-            moveAgents board (unAg : movedAgents) agents calcType
+            moveAgentsOld board (unAg : movedAgents) agents calcType
        in (rBoard, rmovedAgents, unAg : rnotMovedAgents)
     else
       let (rBoard, rmovedAgents, rnotMovedAgents) =
-            moveAgents updBoard (updAgent : movedAgents) agents calcType
+            moveAgentsOld updBoard (updAgent : movedAgents) agents calcType
        in (rBoard, updAgent : rmovedAgents, rnotMovedAgents)
   where
     (updBoard, updAgent) = agentApplyMove board ag act

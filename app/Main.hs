a337dcfd0a741c8a3d040e8713d040b4ff137c88
rodrigo-pino <rodrodpino@gmail.com>
2022-02-03 23:57:55 -0500

    Minor changes, add tested variations

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,21 @@
 module Main where
 
 import Agent.Logic.Pathfinding.PathCalculation
+import Agent.Objects
 import Simulation
 
 main :: IO ()
-main = runSimulation 1 10 BestReward
+main = do
+  -- runSimulation 0 1 Cautious Balance
+  -- runSimulation 0 1 Bold Balance
+  -- runSimulation 1 1 Cautious Balance
+  -- runSimulation 1 1 Bold Balance
+  -- runSimulation 1 10 Cautious ShortestPath
+  -- runSimulation 1 10 Bold ShortestPath
+  -- runSimulation 2 1 Cautious ShortestPath
+  -- runSimulation 2 1 Bold ShortestPath
+  -- runSimulation 2 1 Cautious Balance
+  -- runSimulation 2 1 Bold Balance
+  -- runSimulation 3 5 Bold ShortestPath
+  runSimulation 3 5 Cautious (BalanceCrib Nothing)
+  print "Exit"

5a584f99049ea9ef0d334260f79e5cf2772fbf28
rodrigo-pino <rodrodpino@gmail.com>
2022-02-03 23:52:16 -0500

    Add legend to board display

--- src/Visual.hs
+++ src/Visual.hs
@@ -1,18 +1,27 @@
 module Visual where
 
-import Data.List (sort)
+import Data.List (foldl', nub, sort)
 import Data.Maybe (fromJust)
 import GHC.Base (join)
 import World.Objects
 
-printState board =
+printState board = do
   printBoard (drawBoard board 0)
+  print (drawLegend board)
 
 printBoard :: [[Char]] -> IO ()
 printBoard [] = return ()
 printBoard (row : rows) = do
   print row
   printBoard rows
+
+drawLegend :: Board -> String
+drawLegend board
+  | not (null objTypes) = word ++ foldl' (\acc val -> acc ++ ", " ++ val) "" words
+  | otherwise = ""
+  where
+    objTypes = filter (not . null) ((sort . nub . concat) [getRow board i | i <- [0 .. maxRows board - 1]])
+    (word : words) = [drawChar ots : '-' : '>' : drawTypes ots | ots <- objTypes]
 
 drawBoard :: Board -> Int -> [[Char]]
 drawBoard board i
@@ -47,4 +56,22 @@ drawChar objTypes =
     [Obstacle] -> 'o'
     [Robot Nothing] -> 'r'
     [Robot (Just Kid)] -> 'R'
+    _ -> error ("Invalid combination: " ++ show objTypes)
+
+-- drawTypes :: [ObjectType] -> String
+drawTypes objTypes =
+  case objTypes of
+    [Crib] -> "Crib"
+    [Crib, Kid] -> "Crib with Kid"
+    [Crib, Robot Nothing] -> "Crib and Robot"
+    [Crib, Kid, Robot Nothing] -> "Crib with Kid and Robot"
+    [Crib, Robot (Just Kid)] -> "Crib and Robot with Kid"
+    [Dirt] -> "Dirt"
+    [Dirt, Robot Nothing] -> "Dirt and Robot"
+    [Dirt, Robot (Just Kid)] -> "Dirt and Robot with Kid"
+    [Kid] -> "Kid"
+    [Kid, Robot Nothing] -> "Kid and Robot"
+    [Obstacle] -> "Obstacle"
+    [Robot Nothing] -> "Robot"
+    [Robot (Just Kid)] -> "Robot with Kid"
     _ -> error ("Invalid combination: " ++ show objTypes)

52c5bd2092e8f046385407f40594ba5a9f9c8501
rodrigo-pino <rodrodpino@gmail.com>
2022-02-03 23:53:04 -0500

    Improve and refactor path calculation for performance

--- src/Agent/Logic/Pathfinding/PathCalculation.hs
+++ src/Agent/Logic/Pathfinding/PathCalculation.hs
@@ -1,6 +1,9 @@
 module Agent.Logic.Pathfinding.PathCalculation where
 
 import Agent.Objects
+import Control.Parallel (par, pseq)
+import Data.HashMap.Strict (HashMap (..))
+import qualified Data.HashMap.Strict as Map
 import Data.Maybe (fromJust, isJust, isNothing)
 import Data.Sequence (Seq (..))
 import qualified Data.Sequence as Seq
@@ -10,14 +13,17 @@ import Debug.Trace (trace)
 import GHC.List (foldl')
 import World.Objects
 
-data PathCalcType = ShortestPath | Balance | BalanceCrib
+data PathCalcType
+  = ShortestPath
+  | Balance
+  | BalanceCrib {values :: Maybe (HashMap Position Natural)}
 
 actionCalc :: PathCalcType -> Board -> Maybe Object -> Natural -> Action Position -> Natural
 actionCalc typex board target acc move =
   case typex of
     ShortestPath -> acc + 1
     Balance -> balanceCalc board acc move target
-    BalanceCrib -> balanceCribCalc board acc move target
+    BalanceCrib maybeMap -> balanceCribCalc board acc move target (fromJust maybeMap)
 
 balanceCalc :: Board -> Natural -> Action Position -> Maybe Object -> Natural
 balanceCalc board acc@(Natural val) move target =
@@ -42,44 +48,65 @@ balanceCalc board acc@(Natural val) move target =
             if isJust target
               then distance ((position . fromJust) target) (value move)
               else 0
-       in Natural (baseVal * 10)
+       in Natural (baseVal * 50)
 
 -- Same as Balance, but avoids blocking the cribs
-balanceCribCalc :: Board -> Natural -> Action Position -> Maybe Object -> Natural
-balanceCribCalc board acc mov@(Drop pos) target
+balanceCribCalc :: Board -> Natural -> Action Position -> Maybe Object -> HashMap Position Natural -> Natural
+balanceCribCalc board acc mov@(Drop pos) target valueMaps
   | fromJust (board ! pos) == [Crib, Robot (Just Kid)] =
     let fCost = balanceCalc board acc mov target
-        shDistance =
-          trace
-            ("doing distance thing from" ++ show pos)
-            findFirstNonCrib
-            (Seq.fromList [(pos + dir, 1) | dir <- directions])
-            Set.empty
-     in trace ("cost is " ++ show (fCost, shDistance * 300, fCost - shDistance * 300)) fCost - (shDistance * 300)
+        pCost = fromJust (Map.lookup pos valueMaps)
+     in fCost - (pCost * baseCost)
   | otherwise = balanceCalc board acc mov target
   where
-    -- find shortest path
-    findFirstNonCrib Seq.Empty _ = Infinite
-    findFirstNonCrib ((pos, cost) :<| queue) visited
-      | pos `Set.member` visited
-          || isNothing maybeCell =
-        findFirstNonCrib queue visited
-      | Crib `elem` cell && Kid `notElem` cell = findFirstNonCrib newQueue newVisited
-      | Obstacle `notElem` cell = trace ("Found clean case in" ++ show cell ++ " at: " ++ show pos) cost
-      | otherwise = findFirstNonCrib queue newVisited
+    baseCost = 200
+    totalCribs = Natural (length (getByType board Crib))
+balanceCribCalc board acc mov target _ = balanceCalc board acc mov target
+
+fillValues board (BalanceCrib _) =
+  let cribs = getByType board Crib
+      costs = f cribs
+      map = Map.fromList costs
+   in BalanceCrib (Just map)
+  where
+    f [] = []
+    f (c : cs) = val `par` (vals `pseq` (pos, val) : vals)
       where
-        -- store new position
-        newVisited = pos `Set.insert` visited
-        -- add next possible possitions
-        newQueue =
-          foldl'
-            (\acc val -> acc :|> (val, cost + 1))
-            queue
-            [pos + dir | dir <- directions]
-        -- cache
-        maybeCell = board ! pos
-        cell = trace ("Analyzing pos: " ++ show pos) fromJust maybeCell
-balanceCribCalc board acc mov target = balanceCalc board acc mov target
+        -- recursion
+        vals = f cs
+        -- logic
+        pos = position c
+        val =
+          findFirstNonCrib
+            board
+            (Seq.fromList [(pos + dir, 0) | dir <- directions])
+            Set.empty
+fillValues _ calctype = calctype
+
+findFirstNonCrib _ Seq.Empty _ = Infinite
+findFirstNonCrib board ((pos, cost) :<| queue) visited
+  | pos `Set.member` visited
+      || isNothing maybeCell =
+    findFirstNonCrib board queue visited
+  | Crib `elem` cell && Kid `notElem` cell = findFirstNonCrib board newQueue newVisited
+  | Crib `notElem` cell && Obstacle `notElem` cell = cost
+  | otherwise = findFirstNonCrib board queue newVisited
+  where
+    -- store new position
+    newVisited = pos `Set.insert` visited
+    -- add next possible possitions
+    newQueue =
+      foldl'
+        (\acc val -> acc :|> (val, cost + 1))
+        queue
+        [pos + dir | dir <- directions]
+    -- cache
+    maybeCell = board ! pos
+    cell = fromJust maybeCell
+
+dummyPow num 0 = 1
+dummyPow num 1 = num
+dummyPow num pow = num * dummyPow num (pow - 1)
 
 distance p1 p2 =
   let r = abs (row p1 - row p2)
